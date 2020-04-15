af89311f453e30ae26464a7c144b991d103d8e14
viu <vishnu.kraman88@gmail.com>
2020-04-15 16:05:35 +0530

    toggling selected class

--- movie_seater/app.js
+++ movie_seater/app.js
@@ -2,6 +2,19 @@ const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row.seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-const movieSelect = document.getElementById("movie");
+const movieSelect = document.getElementById("select-movie");
 
 const ticketPrice = +movieSelect.value;
+
+const seatHandler = (e) => {
+  //only selecting seat
+  const target = e.target;
+  if (
+    target.classList.contains("seat") &&
+    !target.classList.contains("occupied")
+  ) {
+    target.classList.toggle("selected");
+  }
+};
+
+container.addEventListener("click", seatHandler);
