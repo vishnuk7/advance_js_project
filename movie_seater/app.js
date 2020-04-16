9441b8afa8e9856862c4c9d4505a10f838b8594a
viu <vishnu.kraman88@gmail.com>
2020-04-16 19:23:50 +0530

    storing selected seat index into array

--- movie_seater/app.js
+++ movie_seater/app.js
@@ -1,18 +1,31 @@
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".row.seat:not(.occupied)");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("select-movie");
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  //copy selected seats into array
+  //Map through array
+  //return a new array of indexes
+
+  const seatIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+
   const selectedSeatsCount = selectedSeats.length;
-  console.log(selectedSeatsCount);
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 };
+
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
 
 const seatHandler = (e) => {
   //only selecting seat
